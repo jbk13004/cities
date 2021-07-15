8db539681d166899fb70a1fd6d9d0ba011b3a44f
jbk13004 <81210692+jbk13004@users.noreply.github.com>
2021-07-15 10:18:37 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,87 @@
+var config = {
+    style: 'mapbox://styles/jbk13004/ckmjqnyt05dyi17qin3m2d9n9',
+    accessToken: 'pk.eyJ1IjoiamJrMTMwMDQiLCJhIjoiY2tpOTVzeWpkMDN4ZTJ5bjg3OTBzN21udiJ9.gJQPNCgsOp2w9ECuu_fj2A',
+    showMarkers: true,
+    markerColor: '#49566E',
+    theme: 'light',
+    use3dTerrain: false,
+    subtitle: 'Towns, cities and counties across the US can help rid the world of nuclear weapons. In addition to lobbying for state and federal legislators to sign the Pledge and work towards alignment with the 2017 UN Treaty on the Prohibition of Nuclear Weapons, NuclearBan.US is asking local governments and town residents to work towards the alignment of their municipalities with the Treaty.',
+    byline: 'Explore the map below to learn about the American towns and cities that have aligned themselves with the Treaty:',
+	chapters: [
+
+	{
+            id: 'other-identifier2',
+            alignment: 'right',
+            hidden: false,
+            title: 'Takoma Park, Maryland',
+            image: 'takoma.jpg',
+			description: 'On March 14, 2018, the City Council of Takoma Park, Maryland voted unanimously on a <a href= "https://documents.takomaparkmd.gov/government/city-council/agendas/2018/council-20180314-2.pdf" target="_blank" rel="noopener noreferrer">resolution</a> that reaffirms the city’s support for the Treaty on the Prohibition of Nuclear Weapons, or ‘Nuclear Ban Treaty.’<p>The Nuclear Ban Treaty was adopted in 2017 at the UN by 122 countries. The Treaty comprehensively prohibits everything to do with nuclear weapons and makes these weapons illegal under international law. Fifty-seven countries have signed it so far, and as many as 146 countries are expected to sign it in the coming months.</p><p>The United States is not one of them, but as with the Paris Climate Agreement, that does not stop cities, states, businesses, colleges and faith communities from complying with this Treaty.</p><p>Takoma Park was one of the first cities in the US to declare itself a ‘Nuclear-Free Zone’ back in 1983. Since then, through a <a href= "http://www.codepublishing.com/MD/TakomaPark/#!/TakomaPark14/TakomaPark1404.html" target="_blank" rel="noopener noreferrer">city ordinance</a>, Takoma Park has banned companies involved in the production of nuclear weapons from doing business in Takoma Park, or from bidding on City contracts, and has avoided investment in companies engaged in the production of nuclear weapons.</p><p>Since these prohibitions mean that Takoma Park already complies with the new Treaty, the March 14 resolution declaring the City ‘treaty compliant’ was more of a formality. As the first city in the US to do so, however, Takoma Park has led the way for many more to follow.</p><p>As many as 220 Nuclear Free Zones were established across the US during the 1980s. Many of these, including Chicago, still have legally-binding ordinances on their books which comply with the new Treaty. Others, including New York City, are now looking into this afresh in light of this Treaty.</p><p>By aligning themselves with the rest of the world in support of the Nuclear Ban Treaty, Takoma Park is turning its back on the $1.2 trillion plans to upgrade and modernize the US nuclear arsenal. Takoma Park is the first to officially do so, but it will not be the last.</p>',
+            //description: 'Berkeley has become the second city in the US to proclaim itself ‘in compliance’ with the Treaty on the Prohibition of Nuclear Weapons, following Takoma Park, Maryland, who made a similar declaration in April of 2018. Takoma Park received their ‘certificate of treaty compliance’ at a small ceremony in the United Nations building on May 14th, 2018.<p>The previous day, the City Council of Berkeley, CA, passed by unanimous consent a <a href= "https://www.cityofberkeley.info/Clerk/City_Council/2018/05_May/Documents/2018-05-15_Item_25_Resolution_for_the_City_of_Berkeley.aspx" target="_blank" rel="noopener noreferrer">resolution </a>declaring their ‘strong support’ for the Treaty on the Prohibition of Nuclear Weapons (TPNW), and calling on cities and states across the country “to give moral leadership by committing to compliance with the Treaty to the extent possible at their respective levels of authority.”</p><p>Through the Nuclear Free Berkeley Act of 1986, Berkeley has maintained one of the strongest municipal prohibitions against the nuclear weapons industry in the country for over thirty years, prohibiting any work on nuclear weapons from taking place within the city, and barring the city from contracting with, or investing in, any company working on nuclear weapons.</p><p>Nevertheless, Berkeley has a complicated legacy that leads many to doubt its sincerity as a nuclear-free city. It played a foundational role in the original development of nuclear weapons, with the discovery of plutonium in Berkeley labs in 1941. The University of California continues to be closely connected to both the Lawrence Livermore National Laboratory and the Los Alamos National Laboratory, key facilities in the ongoing production of nuclear weapons in this country.</p><p>These nuclear weapons laboratories began as off-shoots of the Berkeley campus, but are now run for the federal government by industrial consortiums which include the University of California, but not the Berkeley campus as such. The University of California is run by a board of regents who are appointed by the governor of California.</p><p>So when the City of Berkeley calls on cities and states to comply with the Treaty “to the extent possible at their respective levels of authority,” they mean precisely that: a city can only control what is within its powers to control, and we should not expect otherwise. The Treaty itself calls on countries to implement its provisions “within their jurisdiction and control,” because even sovereign nation-states have limits to their powers.</p><p>The City of Berkeley cannot control the University of California or the nuclear weapons labs that are outside its city limits. What the Nuclear Free Berkeley Act has been able to do for over 30 years, however, is to prohibit any nuclear weapons work from taking place within the city limits of Berkeley – including on the Berkeley campus of the University. It has prevented nuclear weapons companies from getting contracts with the city of Berkeley for other work and it has meant that city funds have not been invested in those companies. That is not an insignificant contribution towards a world without nuclear weapons, and other cities across the US have also done this.</p><p>Now that the City of Berkeley has declared itself in compliance with the Treaty, we need the citizens of Berkeley – and the students of UC Berkeley – to rise up and demand that both the University and the State of California become Treaty Compliant next!</p>',
+            location: {
+                center: [-77.00451077220836, 38.98154287907469],
+                zoom: 9.5,
+                pitch: 50,
+                bearing: -43.2,
+				speed: 0.8
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+		
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Northampton, Massachusetts',
+            image: 'northampton.jpg',
+            description: 'On September 26, 2018, at a ceremony on the steps of Northampton city hall, Northampton Mayor David Narkewicz issued an <a href= "http://northamptonma.gov/DocumentCenter/View/11503/Executive-Policy-Order---Supporting-the-Mitigation--Abolition-of-Nuclear-Weapons---Sept-26-2019?bidId=" target="_blank" rel="noopener noreferrer">executive policy order </a>establishing a city policy barring, to the extent practical and permitted by law, Northampton from investing and contracting with nuclear weapons companies. Under the executive policy order Mayor Narkewicz established a city-wide commitment to “reduce and eventually eliminate any and all ties to the nuclear weapons industry”. The policy order also establishes September 26 as Nuclear Ban Day in Northampton, which the UN designated in 2013 as the International Day for the Total Elimination of Nuclear Weapons. <p>The executive policy order is the culmination of a citizen-led campaign by volunteers of The Resistance Center for Peace and Justice and NuclearBan.US to bring the city into alignment with the 2017 Treaty on the Prohibition of Nuclear Weapons. Mayor Narkewicz’s strong show of support for nuclear weapons elimination and the Nuclear Ban Treaty should serve as a model for other city mayors and elected officials looking to deepen their commitment to achieving a nuclear-free world.</p><p>Northampton has been a municipal leader in the nuclear weapons abolition movement since the 1980s when it became one of the first cities to push for a Nuclear Freeze. In 2005 Northampton joined Mayors for Peace, an international network of cities dedicated to the promotion of peace and nuclear weapons abolition. In 2017 the Northampton City Council passed a city resolution calling on the US government to embrace the Nuclear Ban Treaty and pull back from the brink of nuclear war. Once the city has eliminated their ties to the nuclear weapons industry they will become the third city to officially align with the Nuclear Ban Treaty. Takoma Park, Maryland and Berkeley, California became the first Treaty Aligned cities in early 2018.</p>',
+            location: {
+                center: [-72.64105650630633, 42.32499496334397],
+                zoom: 10,
+                pitch: 40,
+                bearing: 0,
+				speed: 0.8
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'other-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Berkeley, California',
+            image: 'berkeley.jpg',
+            description: 'Berkeley has become the second city in the US to proclaim itself ‘in compliance’ with the Treaty on the Prohibition of Nuclear Weapons, following Takoma Park, Maryland, who made a similar declaration in April of 2018. Takoma Park received their ‘certificate of treaty compliance’ at a small ceremony in the United Nations building on May 14th, 2018.<p>The previous day, the City Council of Berkeley, CA, passed by unanimous consent a <a href= "https://www.cityofberkeley.info/Clerk/City_Council/2018/05_May/Documents/2018-05-15_Item_25_Resolution_for_the_City_of_Berkeley.aspx" target="_blank" rel="noopener noreferrer">resolution</a> declaring their ‘strong support’ for the Treaty on the Prohibition of Nuclear Weapons (TPNW), and calling on cities and states across the country “to give moral leadership by committing to compliance with the Treaty to the extent possible at their respective levels of authority.”</p><p>Through the Nuclear Free Berkeley Act of 1986, Berkeley has maintained one of the strongest municipal prohibitions against the nuclear weapons industry in the country for over thirty years, prohibiting any work on nuclear weapons from taking place within the city, and barring the city from contracting with, or investing in, any company working on nuclear weapons.</p><p>Nevertheless, Berkeley has a complicated legacy that leads many to doubt its sincerity as a nuclear-free city. It played a foundational role in the original development of nuclear weapons, with the discovery of plutonium in Berkeley labs in 1941. The University of California continues to be closely connected to both the Lawrence Livermore National Laboratory and the Los Alamos National Laboratory, key facilities in the ongoing production of nuclear weapons in this country.</p><p>These nuclear weapons laboratories began as off-shoots of the Berkeley campus, but are now run for the federal government by industrial consortiums which include the University of California, but not the Berkeley campus as such. The University of California is run by a board of regents who are appointed by the governor of California.</p><p>So when the City of Berkeley calls on cities and states to comply with the Treaty “to the extent possible at their respective levels of authority,” they mean precisely that: a city can only control what is within its powers to control, and we should not expect otherwise. The Treaty itself calls on countries to implement its provisions “within their jurisdiction and control,” because even sovereign nation-states have limits to their powers.</p><p>The City of Berkeley cannot control the University of California or the nuclear weapons labs that are outside its city limits. What the Nuclear Free Berkeley Act has been able to do for over 30 years, however, is to prohibit any nuclear weapons work from taking place within the city limits of Berkeley – including on the Berkeley campus of the University. It has prevented nuclear weapons companies from getting contracts with the city of Berkeley for other work and it has meant that city funds have not been invested in those companies. That is not an insignificant contribution towards a world without nuclear weapons, and other cities across the US have also done this.</p><p>Now that the City of Berkeley has declared itself in compliance with the Treaty, we need the citizens of Berkeley – and the students of UC Berkeley – to rise up and demand that both the University and the State of California become Treaty Compliant next!</p>',
+            location: {
+                center: [-122.27374928956904, 37.87151872813592],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2,
+				speed: 0.8
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+		
+    ]
+};
